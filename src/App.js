ae566188824b5dfe10d22d058af3943eb719fecc
yjr <emilyryoo5307@gmail.com>
2024-12-21 20:37:55 +0900

    form setting

--- src/App.js
+++ src/App.js
@@ -1,44 +1,28 @@
-//import react from "react";
-//import {Fragment} from "react"
 import { useState } from "react";
 
 function App(){
-  console.log('render');
-  const [number,setNumber]=useState(1);
-  /*
-  [state 이름, set+state이름(함수 명)]=userState(초기값);
-  함수명(값); //함수 사용
-  */
-  //함수 정의 방법1.
-  const double=(number)=>{
-//    number*=number;
-    setNumber(number*2);
-    console.log(number); //
-  };
-
-  const printHello=()=>{
-    console.log('hello')
-  }
-  /*
-  //함수 정의 방법2.
-  const double=function(number){
-    return number*3;
-  }
-  */
+  const[title,setTitle]=useState('');
 
   return(
-    //<Fragment>
-    //<react.Fragment>
-    <>
-      <div>{number}</div>
-      <butten className="btn btn-primary" onClick={double}>
-        Submit
-      </butten>
-    </>
-    //</react.Fragment>
-  );
-    //함수 -> jsx 템플릿 return
-} 
+    //부트스트랩의 container클래스
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input 
+          className="form-control"
+          value={title}
+          onChange={(event)=>{
+            setTitle(event.target.value);
+          }
+          }
+        />
+      </div>
+      <button className="btn btn-primary">
+        Post
+      </button>
+    </div>
 
-export default App; 
-//extport default로 내보낸 이후 다른 곳에서 import 하여 사용
+  );
+}
+
+export default App;
