7c50a96c5d122101ab0233072f3c6622f469d5a6
yjr <emilyryoo5307@gmail.com>
2025-01-11 23:49:05 +0900

    blog list

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,4 +1,11 @@
+import axios
+ from "axios";
 const ListPage=()=>{
+    const getPosts=()=>{
+        axios.get('http://localhost:3001/posts').then((response)=>{
+            console.log(response);
+        })
+    }
     return(
         <div>List Page</div>
     );
