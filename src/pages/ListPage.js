7777e2ae9753c26e74e2e0773ef5c4207b8ce7a4
yjr <emilyryoo5307@gmail.com>
2025-01-12 15:48:40 +0900

    feat: List Page

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,15 +1,28 @@
-import axios
- from "axios";
+import axios from "axios";
+import { useState,useEffect } from "react";
+
 const ListPage=()=>{
+    const [posts,setPosts]=useState([]);
+
     const getPosts=()=>{
         axios.get('http://localhost:3001/posts').then((response)=>{
-            console.log(response);
+            setPosts(response.data);
         })
     }
-    return(
-        <div>List Page</div>
-    );
 
+    useEffect(()=>{
+        getPosts();
+    },[]);
+    return(
+        <div>
+            <h1>Blogs</h1>
+            {posts.map(post=>{
+                return(
+                    <div key={post.id}>{post.title}</div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default ListPage;
